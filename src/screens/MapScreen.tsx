f47a50bde17a6142051963dc312f2b9627941312
Fabio Bergmann <fabiocberg@gmail.com>
2022-02-03 04:55:19 -0300

    feat: macadores, callout, customizações

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,10 +1,38 @@
-import { Dimensions, StyleSheet, View } from "react-native";
-import MapView from "react-native-maps";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import MapView, { Callout, Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import pin from "../../assets/images/pin.png";
+
+const { width, height } = Dimensions.get("window");
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = -27.591724;
+const LONGITUDE = -48.550279;
+const LATITUDE_DELTA = 0.02;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+const INITIAL_REGION = {
+  latitude: LATITUDE,
+  longitude: LONGITUDE,
+  latitudeDelta: LATITUDE_DELTA,
+  longitudeDelta: LONGITUDE_DELTA,
+};
 
 export default function MapScreen() {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        initialRegion={INITIAL_REGION}
+        style={styles.map}
+      >
+        <Marker coordinate={INITIAL_REGION} image={pin}>
+          <Callout>
+            <View>
+              <Text>Janela de informação</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
     </View>
   );
 }
