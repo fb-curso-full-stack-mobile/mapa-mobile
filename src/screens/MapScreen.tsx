df4e74d5c96068a8e8a88c9e75444b7ae89e91c2
Fabio Bergmann <fabiocberg@gmail.com>
2022-02-04 05:24:53 -0300

    feat: polygons

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,5 +1,19 @@
-import { Dimensions, StyleSheet, Text, View } from "react-native";
-import MapView, { Callout, Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { useState } from "react";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import MapView, {
+  Callout,
+  LatLng,
+  MapEvent,
+  Marker,
+  Polygon,
+  PROVIDER_GOOGLE,
+} from "react-native-maps";
 import pin from "../../assets/images/pin.png";
 
 const { width, height } = Dimensions.get("window");
@@ -17,13 +31,41 @@ const INITIAL_REGION = {
   longitudeDelta: LONGITUDE_DELTA,
 };
 
+type MyPolygon = {
+  coordinates: LatLng[];
+};
+
 export default function MapScreen() {
+  const [polygons, setPolygons] = useState<MyPolygon[]>([]);
+  const [editing, setEditing] = useState<MyPolygon | null>();
+
+  const onPress = (e: MapEvent) => {
+    if (!editing) {
+      setEditing({
+        coordinates: [e.nativeEvent.coordinate],
+      });
+    } else {
+      setEditing({
+        ...editing,
+        coordinates: [...editing.coordinates, e.nativeEvent.coordinate],
+      });
+    }
+  };
+
+  const finish = () => {
+    if (editing) {
+      setPolygons([...polygons, editing]);
+      setEditing(null);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <MapView
         provider={PROVIDER_GOOGLE}
         initialRegion={INITIAL_REGION}
         style={styles.map}
+        onPress={onPress}
       >
         <Marker coordinate={INITIAL_REGION} image={pin}>
           <Callout>
@@ -32,7 +74,32 @@ export default function MapScreen() {
             </View>
           </Callout>
         </Marker>
+        {polygons.map((polygon, index) => (
+          <Polygon
+            key={`polygon_${index}`}
+            coordinates={polygon.coordinates}
+            strokeColor="#F00"
+            fillColor="rgba(255,0,0,0.5)"
+            strokeWidth={2}
+          />
+        ))}
+        {editing ? (
+          <Polygon
+            key={`polygon_${polygons.length + 1}`}
+            coordinates={editing.coordinates}
+            strokeColor="#F00"
+            fillColor="rgba(255,0,0,0.5)"
+            strokeWidth={2}
+          />
+        ) : null}
       </MapView>
+      {editing ? (
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={() => finish()}>
+            <Text>Finalizar</Text>
+          </TouchableOpacity>
+        </View>
+      ) : null}
     </View>
   );
 }
@@ -47,5 +114,21 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 0,
+    backgroundColor: "transparent",
+  },
+  button: {
+    width: 90,
+    paddingHorizontal: 12,
+    paddingVertical: 12,
+    borderRadius: 12,
+    borderColor: "black",
+    borderWidth: 2,
+    alignItems: "center",
+    marginHorizontal: 10,
+    backgroundColor: "rgba(255,255,255,0.8)",
   },
 });
